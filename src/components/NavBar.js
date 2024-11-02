1f95c41ba13064da219d96009e86c4ac2d0d1208
jameskimani001 <jameskim092t@gmail.com>
2024-11-02 13:26:51 +0300

    list and keys in react- complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,20 @@
-import React from "react";
+
+import React from 'react';
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = ['home', 'about', 'projects'];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map(function(link, index) {
+        return (
+          <a key={index} href={`#${link}`}>
+            {link}
+          </a>
+        );
+      })}
+    </nav>
+  );
 }
 
 export default NavBar;
